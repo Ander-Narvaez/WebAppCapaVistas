9495e6d0b69bd83ec9d9bde3bd1146b28fc1365b
Ander <anarvaezgo@est.utn.ac.cr>
2021-03-01 15:58:10 -0600

    metodo cargar personas, button agregar, mostras medicos, convert_date

--- WebFormMedicos.aspx.cs
+++ WebFormMedicos.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,7 @@ namespace WebAppCapaPersonas
     public partial class WebFormMedicos : System.Web.UI.Page
     {
         ServiceReference.WebServiceSoapClient Ws = new ServiceReference.WebServiceSoapClient();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             String vResfrescarObjeto;
@@ -21,16 +23,39 @@ namespace WebAppCapaPersonas
             }
             else
             {
-                //MostrarMedicos();
-                //Solo se ejecuta unavez 
+                MostrarMedicos();
             }
         }
 
+        public DateTime Convert_Date(String pFecha)
+        {
+            return DateTime.Parse(pFecha);
+        }
 
+        public void MostrarMedicos()
+        {
+            DataSet Dts = new DataSet();
+            Dts = Ws.GetListaMedicos("",DateTime.MinValue,DateTime.MinValue,"" ,"", "S");
+            GridViewMedicos.DataSource = Dts;
+            GridViewMedicos.DataBind();
+
+            DropDownListNif.DataSource = Cargar_Personas();
+            DropDownListNif.DataTextField = "NIF";
+            //DropDownListNif.DataValueField = "Cedula";
+            DropDownListNif.DataBind();
+        }
+
+        public DataSet Cargar_Personas ()
+        {
+            DataSet Dts = new DataSet();
+            Dts = Ws.GetListaPersonas("","","","","","","","", "Q");
+            
+            return Dts;
+        }
         protected void BtnAgregar_Click(object sender, EventArgs e)
         {
-             Labelmsg.Text = Ws.MantenimientoMedico(DropDownListNif.SelectedItem.ToString(), fecha_alta.Value,fecha_baja.Value, inputNCO.Text, inputState.SelectedItem.ToString(), "I");
-            //MostrarMedicos();
+            Labelmsg.Text = Ws.MantenimientoMedico(DropDownListNif.SelectedItem.ToString(), Convert_Date(fecha_alta.Value), Convert_Date(fecha_baja.Value), inputNCO.Text, inputState.SelectedItem.ToString(), "I");
+            MostrarMedicos();
         }
     }
 }
